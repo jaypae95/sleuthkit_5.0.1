1dccb6deaa28d1bf47da14722e306e69cab8dc6c
kimminki10 <pangshe10@gmail.com>
2018-11-14 00:20:01 +0900

    xfs_open

--- tsk/fs/xfs.c
+++ tsk/fs/xfs.c
@@ -1,7 +1,7 @@
 #include "tsk_fs_i.h"
 
 TSK_FS_INFO *
-hfs_open(TSK_IMG_INFO * img_info, TSK_OFF_T offset,
+xfs_open(TSK_IMG_INFO * img_info, TSK_OFF_T offset,
     TSK_FS_TYPE_ENUM ftype, uint8_t test) {
         
         printf("hello tsk I'm mingi\n");
