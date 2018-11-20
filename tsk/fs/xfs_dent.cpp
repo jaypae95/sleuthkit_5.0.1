d4f7985fd59ea76560334e4212c70a5dbcd8ab7d
jaypae95 <jaypae95@naver.com>
2018-11-20 04:11:01 -0800

    dashi

--- tsk/fs/xfs_dent.cpp
+++ tsk/fs/xfs_dent.cpp
@@ -72,70 +72,70 @@ xfs_dir_open_meta(TSK_FS_INFO * a_fs, TSK_FS_DIR ** a_fs_dir,
 // #endif
 //     }
 
-    if ((fs_dir->fs_file =
-            tsk_fs_file_open_meta(a_fs, NULL, a_addr)) == NULL) {
-        tsk_error_reset();
-        tsk_error_errstr2_concat("- xfs_dir_open_meta");
-        return TSK_COR;
-    }
+    // if ((fs_dir->fs_file =
+    //         tsk_fs_file_open_meta(a_fs, NULL, a_addr)) == NULL) {
+    //     tsk_error_reset();
+    //     tsk_error_errstr2_concat("- xfs_dir_open_meta");
+    //     return TSK_COR;
+    // }
 
-    // We only read in and process a single block at a time
-    if ((dirbuf = tsk_malloc((size_t)a_fs->block_size)) == NULL) {
-        return TSK_ERR;
-    }
-    
-    size = roundup(fs_dir->fs_file->meta->size, a_fs->block_size);
-    TSK_OFF_T offset = 0;
+    // // We only read in and process a single block at a time
+    // if ((dirbuf = tsk_malloc((size_t)a_fs->block_size)) == NULL) {
+    //     return TSK_ERR;
+    // }
 
-    while (size > 0) {
-        ssize_t len = (a_fs->block_size < size) ? a_fs->block_size : size;
-        ssize_t cnt = tsk_fs_file_read(fs_dir->fs_file, offset, dirbuf, len, (TSK_FS_FILE_READ_FLAG_ENUM)0);
-        if (cnt != len) {
-            tsk_error_reset();
-            tsk_error_set_errno(TSK_ERR_FS_FWALK);
-            tsk_error_set_errstr
-            ("ext2fs_dir_open_meta: Error reading directory contents: %"
-                PRIuINUM "\n", a_addr);
-            free(dirbuf);
-            return TSK_COR;
-        }
+    // size = roundup(fs_dir->fs_file->meta->size, a_fs->block_size);
+    // TSK_OFF_T offset = 0;
 
-        retval_tmp =
-            ext2fs_dent_parse_block(ext2fs, fs_dir,
-            (fs_dir->fs_file->meta->
-                flags & TSK_FS_META_FLAG_UNALLOC) ? 1 : 0, &list_seen,
-            dirbuf, len);
+    // while (size > 0) {
+    //     ssize_t len = (a_fs->block_size < size) ? a_fs->block_size : size;
+    //     ssize_t cnt = tsk_fs_file_read(fs_dir->fs_file, offset, dirbuf, len, (TSK_FS_FILE_READ_FLAG_ENUM)0);
+    //     if (cnt != len) {
+    //         tsk_error_reset();
+    //         tsk_error_set_errno(TSK_ERR_FS_FWALK);
+    //         tsk_error_set_errstr
+    //         ("ext2fs_dir_open_meta: Error reading directory contents: %"
+    //             PRIuINUM "\n", a_addr);
+    //         free(dirbuf);
+    //         return TSK_COR;
+    //     }
 
-        if (retval_tmp == TSK_ERR) {
-            retval_final = TSK_ERR;
-            break;
-        }
-        else if (retval_tmp == TSK_COR) {
-            retval_final = TSK_COR;
-        }
+    //     retval_tmp =
+    //         ext2fs_dent_parse_block(ext2fs, fs_dir,
+    //         (fs_dir->fs_file->meta->
+    //             flags & TSK_FS_META_FLAG_UNALLOC) ? 1 : 0, &list_seen,
+    //         dirbuf, len);
 
-        size -= len;
-        offset += len;
-    }
-    free(dirbuf);
+    //     if (retval_tmp == TSK_ERR) {
+    //         retval_final = TSK_ERR;
+    //         break;
+    //     }
+    //     else if (retval_tmp == TSK_COR) {
+    //         retval_final = TSK_COR;
+    //     }
 
-    // if we are listing the root directory, add the Orphan directory entry
-    if (a_addr == a_fs->root_inum) {
-        TSK_FS_NAME *fs_name = tsk_fs_name_alloc(256, 0);
-        if (fs_name == NULL)
-            return TSK_ERR;
+    //     size -= len;
+    //     offset += len;
+    // }
+    // free(dirbuf);
 
-        if (tsk_fs_dir_make_orphan_dir_name(a_fs, fs_name)) {
-            tsk_fs_name_free(fs_name);
-            return TSK_ERR;
-        }
+    // // if we are listing the root directory, add the Orphan directory entry
+    // if (a_addr == a_fs->root_inum) {
+    //     TSK_FS_NAME *fs_name = tsk_fs_name_alloc(256, 0);
+    //     if (fs_name == NULL)
+    //         return TSK_ERR;
 
-        if (tsk_fs_dir_add(fs_dir, fs_name)) {
-            tsk_fs_name_free(fs_name);
-            return TSK_ERR;
-        }
-        tsk_fs_name_free(fs_name);
-    }
+    //     if (tsk_fs_dir_make_orphan_dir_name(a_fs, fs_name)) {
+    //         tsk_fs_name_free(fs_name);
+    //         return TSK_ERR;
+    //     }
 
-    return retval_final;
+    //     if (tsk_fs_dir_add(fs_dir, fs_name)) {
+    //         tsk_fs_name_free(fs_name);
+    //         return TSK_ERR;
+    //     }
+    //     tsk_fs_name_free(fs_name);
+    // }
+
+    // return retval_final;
 }
